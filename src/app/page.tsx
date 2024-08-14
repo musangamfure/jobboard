82ea10999c16baea18ba49f452c219084f4c05d1
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-08-14 15:59:00 +0200

    Add Jobfilter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import prisma from "@/lib/prisma";
 import JobListItem from "@/components/JobListItem";
+import JobFilterSideBar from "@/components/JobFilterSideBar";
 
 export default async function Home() {
   const jobs = await prisma.job.findMany({
@@ -18,10 +19,16 @@ export default async function Home() {
         <h1 className="text-4xl font-extrabold tracking-tight lg:text-5xl">
           Develop Jobs
         </h1>
+        <p className="text-muted-foreground"> Find your dream job.</p>
       </div>
-      {jobs.map((job) => (
-        <JobListItem key={job.id} job={job} />
-      ))}
+      <section className="flex flex-col  md:flex-row gap-4">
+        <JobFilterSideBar />
+        <div className="grow space-x-4">
+          {jobs.map((job) => (
+            <JobListItem key={job.id} job={job} />
+          ))}
+        </div>
+      </section>
     </main>
   );
 }
