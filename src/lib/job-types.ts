083aeef0efc5e84435bbca64bc885aec7b2d53c3
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-08-14 16:01:40 +0200

    Create job type  arrays

---
+++ src/lib/job-types.ts
@@ -0,0 +1,11 @@
+export const jobTypes = [
+  "Full-time",
+  "Part-time",
+  "Internship",
+  "Temporary",
+  "Contract",
+  "Permanent",
+  "Volunteer",
+];
+
+export const locattionTypes = ["Remote", "on-site", "Hybrid"];
