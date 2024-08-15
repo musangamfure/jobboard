87e37b3321c6c5c203b75012960f4b75d8e8fc20
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-08-15 23:57:50 +0200

    Add Dynamic page Title and metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import JobFilterSideBar from "@/components/JobFilterSideBar";
 import H1 from "@/components/ui/h1";
 import JobResults from "@/components/JobFilterResults";
 import { JobFilterValues } from "@/lib/validation";
+import { Metadata } from "next";
 
 interface PageProps {
   searchParams: {
@@ -12,6 +13,37 @@ interface PageProps {
     location?: string;
     remote?: string;
     type?: string;
+  };
+}
+
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+    ? `${type} jobs`
+    : location
+    ? `${location} jobs`
+    : remote
+    ? "Remote jobs"
+    : "All developer jobs";
+
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
+}
+
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: PageProps): Metadata {
+  const filterValues: JobFilterValues = {
+    q,
+    type,
+    location,
+    remote: remote === "true",
+  };
+
+  return {
+    title: `${getTitle(filterValues)} | Flow Jobs`,
   };
 }
 
@@ -28,7 +60,7 @@ export default async function Home({
   return (
     <main className="m-auto max-w-5xl my-10 space-y-10 px-3">
       <div className="space-y-5 text-center">
-        <H1 />
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground"> Find your dream job.</p>
       </div>
       <section className="flex flex-col  md:flex-row gap-4">
