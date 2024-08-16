d8e0197b5ffc0812fa1a0e759f6cdc4ffe833b80
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-08-16 20:28:07 +0200

    Add slug help function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,10 @@ export function relativeDate(from: Date) {
     addSuffix: true,
   });
 }
+
+export function toSlug(str: string) {
+  return str
+    .toLowerCase()
+    .replace(/[^\w ]+/g, "")
+    .replace(/ +/g, "-");
+}
