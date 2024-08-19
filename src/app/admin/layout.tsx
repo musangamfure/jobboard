4d2beba207375f9cbe1a8b62778a92a8a53d16a0
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-08-19 23:27:20 +0200

    Add admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,5 +6,5 @@ export const metadata: Metadata = {
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return <>{children}</>;
 }
